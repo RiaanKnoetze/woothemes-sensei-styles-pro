e0f1c8f3fdf7c12a3fa11a50132a9c187141a8e3
RiaanKnoetze <riaan@woothemes.com>
2016-05-24 14:35:30 +0200

    Add theme name as body class for extra special styling.

--- includes/hooks/template.php
+++ includes/hooks/template.php
@@ -28,11 +28,23 @@ function sensei_pro_enqueue_styles() {
 
 
 // Replace default placeholder imager
-function get_new_placeholder() {
+add_filter('sensei_course_placeholder_image_url', 'sensei_styles_get_new_placeholder');
+add_filter('sensei_lesson_placeholder_image_url', 'sensei_styles_get_new_placeholder');
+
+function sensei_styles_get_new_placeholder() {
 	global $sensei_styles_dir;
 
 	$placeholder_url = '<img src="' . $sensei_styles_dir . 'assets/images/placeholder.png" class="woo-image thumbnail alignleft" />';
 	return $placeholder_url;
 }
-add_filter('sensei_course_placeholder_image_url', 'get_new_placeholder');
-add_filter('sensei_lesson_placeholder_image_url', 'get_new_placeholder');
+
+
+
+// Add theme name as body class for extra special styling
+add_filter( 'body_class', 'sensei_styles_get_theme_name' );
+
+function sensei_styles_get_theme_name( $classes ) {
+    $sensei_template = get_option( 'template' );
+    $classes[] = 'sensei-'. $sensei_template;
+    return $classes;
+}
