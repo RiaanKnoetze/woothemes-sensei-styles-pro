8bc77ae341ad441ce43f8cbb663438c7fc094bcc
RiaanKnoetze <riaan@woothemes.com>
2016-05-25 11:45:14 +0200

    New Progress indicator + empty results heading "fix".

--- assets/js/sensei-styles-pro.js
+++ assets/js/sensei-styles-pro.js
@@ -1,4 +1,11 @@
 jQuery(document).ready(function($) {
 	// Add a special classname to all courses that contain a featured course image
 	jQuery('#main-course .course .woo-image').parent().addClass('course-featured-image');
+
+	// Hide empty heading tags on results page
+	jQuery('#main .course-results-lessons h2, #main .course-results-lessons h3')
+	.filter(function() {
+	  var text = $(this).text().replace(/\s*/g, '');
+	  return !text;
+	}).hide();
 });
