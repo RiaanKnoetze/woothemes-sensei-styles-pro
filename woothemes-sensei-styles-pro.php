a98c769d2215d69fd97cb4bffab2a220747bf9b0
rspublishing <rynaldo.stoltz@automattic.com>
2017-10-24 14:07:34 +0200

    check if Sensei is active

--- woothemes-sensei-styles-pro.php
+++ woothemes-sensei-styles-pro.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: Sensei Styles Pro
-Plugin URI: http://www.woothemes.com/products/sensei/
+Plugin URI: http://www.woocommerce.com/products/sensei/
 Description: A plugin that upgrades the default Sensei styles.
 Version: 1.0
 Author: WooThemes
-Author URI: http://www.woothemes.com/
+Author URI: http://www.woocommerce.com/
 License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 Requires at least: 4.1
 Tested up to: 4.5
@@ -34,9 +34,21 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
     require_once( 'includes/hooks/template.php' );
 
+    // Check if Sensei is active
+if ( ! in_array( 'sensei/woothemes-sensei.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+    add_action( 'admin_notices', 'nosensei_admin_notice' );
+    
+} else {
+
     /**
      * Returns the global Sensei Styles Instance.
      */
     function Sensei_Styles(){
         return Sensei_Styles_Main::instance();
     }
+}
+
+// Show notice until Sensei is activated
+function nosensei_admin_notice() {
+    echo '<div class="error"><p>' . sprintf( __( 'Sensei Styles Pro requires the %s to be installed and active.', 'woothemes-sensei-styles-pro' ), '<a href="https://woocommerce.com/products/sensei/" target="_blank">Sensei plugin</a>' ) . '</p></div>';
+}
