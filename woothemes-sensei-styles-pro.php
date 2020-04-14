d758672b009319911d2ac1709c3009e357817d3a
RiaanKnoetze <RiaanKnoetze@users.noreply.github.com>
2020-04-14 07:26:03 +0100

    Update woothemes-sensei-styles-pro.php

--- woothemes-sensei-styles-pro.php
+++ woothemes-sensei-styles-pro.php
@@ -3,7 +3,7 @@
 Plugin Name: Sensei Styles Pro
 Plugin URI: http://www.woocommerce.com/products/sensei/
 Description: A plugin that upgrades the default Sensei styles.
-Version: 1.0
+Version: 1.0.1
 Author: WooThemes
 Author URI: http://www.woocommerce.com/
 License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
